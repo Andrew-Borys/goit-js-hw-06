f5164cc23328c39722169e6205db04b5012eac84
Andrew Borys <borysandrey1988@gmail.com>
2021-12-13 00:27:20 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,29 @@
 const listOfcategories = document.querySelector("#categories");
 console.log("Number of categories: ", listOfcategories.children.length);
 
-console.log("Category: ", listOfcategories.children[0].children[0].textContent);
+console.log(
+  "Category: ",
+  listOfcategories.children[0].childNodes[1].textContent
+);
 console.log(
   "Elements: ",
-  listOfcategories.children[0].children[1].children.length
+  listOfcategories.children[0].childNodes[3].children.length
 );
 
-console.log("Category: ", listOfcategories.children[1].children[0].textContent);
+console.log(
+  "Category: ",
+  listOfcategories.children[1].childNodes[1].textContent
+);
 console.log(
   "Elements: ",
-  listOfcategories.children[1].children[1].children.length
+  listOfcategories.children[1].childNodes[3].children.length
 );
 
-console.log("Category: ", listOfcategories.children[2].children[0].textContent);
+console.log(
+  "Category: ",
+  listOfcategories.children[2].childNodes[1].textContent
+);
 console.log(
   "Elements: ",
-  listOfcategories.children[2].children[1].children.length
+  listOfcategories.children[2].childNodes[3].children.length
 );
